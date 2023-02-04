299fc6b91e5dd55540cd606a7090c5a65b80296e
CharlieRosander <112691863+CharlieRosander@users.noreply.github.com>
2023-02-04 10:44:17 +0100

    asd

--- main_backup.py
+++ main_backup.py
@@ -2,54 +2,75 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-class BestFitLine:
-    df = pd.read_csv("smhi-data-mod.csv")
+# Class for the raw data and the creation of matrices with that data
+class RawData:
+    df = None
     x = None
     y = None
 
     @classmethod
-    def create_matrix(cls):
-        cls.x = np.array(BestFitLine.df.temp)
-        cls.y = np.array(range(1, len(BestFitLine.df.month) + 1), dtype=float)
-        return cls.x, cls.y
+    def import_csv(cls, csv_file):
+        cls.df = pd.read_csv(csv_file)
+
+    @classmethod
+    def create_matrix(cls, df_x, df_y):
+        cls.x = np.array(df_x)
+
+        for data_type in df_y:
+            if isinstance(data_type, str):
+                cls.y = np.array(range(1, len(df_y) + 1), dtype=float)
+
+                return print(f"The data is of type 'string', filling array with floats for range(len(df_y)).")
+            elif isinstance(data_type, float) or isinstance(data_type, int):
+                cls.y = np.array(df_y, dtype=float)
+
+                return print(f"The data is of type: {type(data_type)}, converted values to dtype float.")
 
 
-BestFitLine.create_matrix()
+class Calculations:
+    rows = None
+    cols = None
+    ab_norm = None
+    k = None
+    m = None
 
-A = np.array([BestFitLine.x, np.ones(len(BestFitLine.x))], dtype=float).T
-b = BestFitLine.y.reshape(-1, 1)
+    @classmethod
+    # Tar in x och y från rå-data som parameter för att skapa matriser som vi kan göra beräkningar på
+    def prepare_arrays(cls, x, y):
+        a = np.array([x, np.ones(len(x))], dtype=float).T
+        b = y.reshape(-1, 1)
 
-# Skapa normalekvationen
-A_trans = A.T
-A = np.dot(A_trans, A)
-b = np.dot(A_trans, b)
+        # Skapa normalekvationen
+        a_trans = a.T
+        a = np.dot(a_trans, a)
+        b = np.dot(a_trans, b)
 
-Ab_norm = np.concatenate((A, b), axis=1)
-rows = np.shape(Ab_norm)[0]
-cols = np.shape(Ab_norm)[1]
+        cls.ab_norm = np.concatenate((a, b), axis=1)
+        cls.rows = np.shape(cls.ab_norm)[0]
+        cls.cols = np.shape(cls.ab_norm)[1]
 
-x = np.zeros(cols - 1)
+    @classmethod
+    def gauss_and_least_square(cls):
+        solution_vector = np.zeros(cls.cols - 1)
+        for i in range(cls.cols - 1):
+            for j in range(i + 1, cls.rows):
+                cls.ab_norm[j, :] = -(cls.ab_norm[j, i] / cls.ab_norm[i, i]) * cls.ab_norm[i, :] + cls.ab_norm[j, :]
 
-for i in range(cols - 1):
-    for j in range(i + 1, rows):
-        Ab_norm[j, :] = -(Ab_norm[j, i] / Ab_norm[i, i]) * Ab_norm[i, :] + Ab_norm[j, :]
+        # Backwards substitution
+        for i in np.arange(cls.rows - 1, -1, -1):
+            solution_vector[i] = (cls.ab_norm[i, -1] - np.dot(cls.ab_norm[i, 0:cls.cols - 1], solution_vector)) \
+                                 / cls.ab_norm[i, i]
 
-for i in np.arange(rows - 1, -1, -1):
-    x[i] = (Ab_norm[i, -1] - np.dot(Ab_norm[i, 0:cols - 1], x)) / Ab_norm[i, i]
-
-k = np.round(x[0], 3)
-m = np.round(x[1], 3)
+        cls.k = np.round(solution_vector[0], 3)
+        cls.m = np.round(solution_vector[1], 3)
 
 
-def create_graph():
+def create_graph(x_raw, y_raw, k, m):
     plt.title("Medeltemperaturer 2022")
     plt.xlabel("Temperature")
     plt.ylabel("Month")
-    plt.scatter(BestFitLine.x, BestFitLine.y, label="Medeltemperatur")
-    plt.plot(BestFitLine.x, k * BestFitLine.x + m, "-r", label=f"Best line fit\nwhere;\nK = {k}\nm = {m}")
+    plt.scatter(x_raw, y_raw, label="Medeltemperatur")
+    plt.plot(x_raw, k * x_raw + m, "-r",
+             label=f"Best line fit\nwhere;\nK = {k}\nm = {m}")
     plt.legend()
     return plt.show()
-
-
-create_graph()
